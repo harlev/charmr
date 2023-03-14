b07c8169877bb092107bde17093652fee017f5c4
Ron Harlev <harlev@gmail.com>
2023-03-14 16:21:54 -0700

    alias system working

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from dotenv import load_dotenv
 import ai
 import argparse
 from load_code import run_function
+import alias_manager
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Charmr - Magical File Converter')
     parser.add_argument('--input_file', '-i', type=str, help='Input file', required=True)
     parser.add_argument('--output_file', '-o', type=str, help='Output file', required=True)
-    parser.add_argument('--conversion', '-c', type=str, help='Conversion description', required=True)
+    parser.add_argument('--conversion', '-c', type=str, help='Conversion description', required=False)
     parser.add_argument('--alias', '-a', type=str, help='Conversion alias, for reuse', required=False)
     #TODO add "inspect only" option
     args = parser.parse_args()
@@ -31,7 +32,17 @@ def main():
     args = parse_arguments()
 
     input_file_content = read_input_file(args.input_file)
-    conversion_code = ai.get_code(args.conversion)
+
+    if args.conversion:
+        conversion_code = ai.get_code(args.conversion)
+        if args.alias:
+            alias_manager.save_code(conversion_code, args.alias)
+    else:
+        if not args.alias:
+            raise Exception("Alias must be provided when no conversion defined")
+        else:
+            conversion_code = alias_manager.load_code(args.alias)
+
     output_file_content = run_function(conversion_code, input_file_content)
     write_output_file(args.output_file, output_file_content)
 
