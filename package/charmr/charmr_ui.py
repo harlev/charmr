87eef92334d900dfb6f4b80877b0f9bfcbfded1e
Ron Harlev <harlev@gmail.com>
2023-05-06 22:55:59 -0700

    hide "running"

--- package/charmr/charmr_ui.py
+++ package/charmr/charmr_ui.py
@@ -12,9 +12,27 @@ hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
+        header {visibility: hidden; height: 0%;}
+        <style>
+        div[data-testid="stToolbar"] {
+        visibility: hidden;
+        height: 0%;
+        position: fixed;
+        }
+        div[data-testid="stDecoration"] {
+        visibility: hidden;
+        height: 0%;
+        position: fixed;
+        }
+        div[data-testid="stStatusWidget"] {
+        visibility: hidden;
+        height: 0%;
+        position: fixed;
+        }
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 st.markdown("""
 <!-- Google tag (gtag.js) -->
